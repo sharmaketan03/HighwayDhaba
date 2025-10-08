19e3764d69ec9f9f20ed991ebc29e0e229f0d010
sharmaketan03 <ketan301024@gmail.com>
2025-10-08 12:11:31 +0530

    Latest

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ProjectHotel/",
+  base: "./", // local + deployment dono ke liye safe base path
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    // hashing disable karne ke liye
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[ext]",
+        chunkFileNames: "[name].js",
+        entryFileNames: "[name].js",
+      },
+    },
   },
 });
