3bd800c8ef669e30ca7e02cc4f818647dd65f682
sharmaketan03 <ketan301024@gmail.com>
2025-10-08 10:38:44 +0530

    trying to many tyms to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // absolute base path for vercel
+  base: "/ProjectHotel/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
